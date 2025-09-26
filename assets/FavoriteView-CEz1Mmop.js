2ad11e2a92e3a58380e5fd594d9a0a49e275a44e
xiaozhangnike1 <xiaozhangnike1@gmail.com>
2025-09-26 16:58:40 +0800

    Add files via upload

---
+++ assets/FavoriteView-CEz1Mmop.js
@@ -0,0 +1,1 @@
+import{_ as e,b as c,o}from"./index-D-goK5yG.js";const r={};function t(n,a){return o(),c("div")}const _=e(r,[["render",t]]);export{_ as default};
---
+++ assets/use-id-BNsXtNVp.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-D-goK5yG.js";let r=0;function u(){const n=e(),{name:t="unknown"}=n?.type||{};return`${t}-${++r}`}export{u};
